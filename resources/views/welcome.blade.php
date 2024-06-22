8337b7cc8bc6772250a67ded41c30d45fa10cc42
hanan3889 <rebaia_hanan@yahoo.fr>
2024-06-22 14:47:09 +0200

    Update img path and move CSS file to public folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <div class="row row-cols-1 row-cols-md-3 g-4">
         <div class="col">
             <div class="card">
-                <img class="card-img-top" src="{{asset('images/recettes/salade-nicoise.jpg')}}" alt="salade-nicoise">
+                <img class="card-img-top" src="{{asset('assets/recettes/salade-nicoise.jpg')}}" alt="salade-nicoise">
                 <div class="card-body">
                     <h5 class="card-title">Salade nicoise</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -19,7 +19,7 @@
         </div>
         <div class="col">
             <div class="card">
-                <img class="card-img-top" src="{{asset('images/recettes/chili-con-carne.jpg')}}" alt="chili-con-carne">
+                <img class="card-img-top" src="{{asset('assets/recettes/chili-con-carne.jpg')}}" alt="chili-con-carne">
                 <div class="card-body">
                     <h5 class="card-title">Chili con carne</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -29,7 +29,7 @@
         </div>
         <div class="col">
             <div class="card">
-                <img class="card-img-top" src="{{asset('images/recettes/sorbet.jpg')}}" alt="sorbet-pamplemousse">
+                <img class="card-img-top" src="{{asset('assets/recettes/sorbet.jpg')}}" alt="sorbet-pamplemousse">
                 <div class="card-body">
                     <h5 class="card-title">Sorbet pamplemousse</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -39,7 +39,7 @@
         </div>
         <div class="col">
             <div class="card">
-                <img class="card-img-top" src="{{asset('images/recettes/toast-avocat.jpg')}}" alt="toast-avocat">
+                <img class="card-img-top" src="{{asset('assets/recettes/toast-avocat.jpg')}}" alt="toast-avocat">
                 <div class="card-body">
                     <h5 class="card-title">Toast d'avocat</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -49,7 +49,7 @@
         </div>
         <div class="col">
             <div class="card">
-                <img class="card-img-top" src="{{asset('images/recettes/veloute.jpg')}}" alt="veloute-champignons">
+                <img class="card-img-top" src="{{asset('assets/recettes/veloute.jpg')}}" alt="veloute-champignons">
                 <div class="card-body">
                     <h5 class="card-title">Velouté de champignons</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -59,7 +59,7 @@
         </div>
         <div class="col">
             <div class="card">
-                <img class="card-img-top" src="{{asset('images/recettes/hamburger.jpg')}}" alt="hamburger">
+                <img class="card-img-top" src="{{asset('assets/recettes/hamburger.jpg')}}" alt="hamburger">
                 <div class="card-body">
                     <h5 class="card-title">Hamburger et frites maison</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
